629dd80ea295dcad028ff5ab441d268f12020376
Milan <nekoshark1156@gmail.com>
2023-11-22 06:00:11 -0800

    s

--- Nový Text Document.js
+++
@@ -1,9 +0,0 @@
-let userName = prompt ("What your name")
-let age = prompt("Please enter your age:");
-if (age >= 18) {
-    alert("Hello, " + userName + " You are 18 or higher");
-    window.location.href = "Nový Text Document.html";
-} else {
-    alert("Hello, " + userName + " You are underage");
-    window.location.href = "index.html";
-} 
